cf951bfa4b6d8260ca2843055f2c4509a5a58a32
bhardwajdhruv <dhruvbhardwaj17@gmail.com>
2026-09-13 18:26:40 +0530

    feat: Clerk auth with sign-in, sign-up and protected watchlist route

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css"
 
 export const metadata = {
@@ -7,15 +8,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <div className="orb orb-1" />
-        <div className="orb orb-2" />
-        <div className="orb orb-3" />
-        <div style={{ position: "relative", zIndex: 1 }}>
-          {children}
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <div className="orb orb-1" />
+          <div className="orb orb-2" />
+          <div className="orb orb-3" />
+          <div style={{ position: "relative", zIndex: 1 }}>
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
